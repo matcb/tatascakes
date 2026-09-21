899d410886f8062f1dcd2fc675966118d4a17836
Matheus Barreto <matcb@users.noreply.github.com>
2026-09-21 20:00:37 -0300

    fix: increase subtitle font size on mobile

--- tatascake-frontend/src/components/FomularioComponent/FormularioComponent.tsx
+++ tatascake-frontend/src/components/FomularioComponent/FormularioComponent.tsx
@@ -126,7 +126,7 @@ export const FormularioBolo = () => {
         <div className="flex flex-col items-center mb-8">
           <Link to="/" className="block mx-auto">
             <img src={logo} alt="Logo Tatas Cake" className="w-32 h-32 sm:w-40 sm:h-40 md:w-50 md:h-50 lg:w-75 lg:h-75 object-contain mx-auto" />
-            <p className="font-accent text-red-font -mt-4 sm:-mt-6 md:-mt-10 lg:-mt-16 text-[1rem] sm:text-[1.2rem] text-center">
+            <p className="font-accent text-red-font -mt-4 sm:-mt-6 md:-mt-10 lg:-mt-16 text-[1.2rem] sm:text-[1.4rem] text-center">
               Confeitaria Artesanal - by R.Cakes
             </p>
           </Link>
