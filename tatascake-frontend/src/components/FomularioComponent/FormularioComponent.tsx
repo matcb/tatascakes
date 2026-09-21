c250c21d4669918a604a187bc6242798ef00acf1
Matheus Barreto <matcb@users.noreply.github.com>
2026-09-21 19:21:53 -0300

    fix: adjust width

--- tatascake-frontend/src/components/FomularioComponent/FormularioComponent.tsx
+++ tatascake-frontend/src/components/FomularioComponent/FormularioComponent.tsx
@@ -131,7 +131,7 @@ export const FormularioBolo = () => {
           </Link>
         </div>
 
-        <FieldSet className="w-full max-w-md">
+        <FieldSet className="w-full">
           {erro && (
             <div className="mb-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded-lg text-center font-medium">
               {erro}
